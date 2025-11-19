23acc8eab27da974ee52e90deade21427dee59f5
Arijit Dasgupta <arijitdg@mit.edu>
2025-11-19 06:35:30 +0000

    edited init model

--- src/jtap/model/initialization.py
+++ src/jtap/model/initialization.py
@@ -2,7 +2,7 @@ import jax
 import genjax
 import jax.numpy as jnp
 from genjax import gen
-from jtap.distributions import uniformposition2d
+from jtap.distributions import uniformcat
 from jtap.utils import ModelOutput
 from .scene_geometry import get_edges_from_scene, get_corners_from_scene, is_ball_fully_hidden, is_ball_fully_visible
 
@@ -10,19 +10,22 @@ from .scene_geometry import get_edges_from_scene, get_corners_from_scene, is_bal
 @gen
 def init_model(mi):
     diameter = mi.diameter
-    masked_barriers = mi.masked_barriers
     masked_occluders = mi.masked_occluders
-    edgemap = mi.edgemap
-    cornermap = mi.cornermap
 
-    x, y = uniformposition2d(jnp.float32(0.), mi.scene_dim[0] - diameter, jnp.float32(0.), mi.scene_dim[1] - diameter) @ "xy" # joint sampling of x and y
+    x = genjax.uniform(jnp.float32(0.), mi.scene_dim[0] - diameter) @ "x"
+    # for the Left-Right task, scene_dim[1] is equal to the diameter of the ball
+    # hence y will always be sampled as 0
+    y = genjax.uniform(jnp.float32(0.), mi.scene_dim[1] - diameter) @ "y"
+
     speed = genjax.uniform(jnp.float32(0.), mi.max_speed) @ "speed"
-    direction = genjax.uniform(-jnp.pi, jnp.pi) @ "direction"
+    direction = uniformcat([-1.0, 1.0]) @ "direction" # -1 is left, 1 is right
 
-    # bug workaround for not being able to return non-traced values for importance
-    collision_branch = jax.lax.select(True, 4., 4.).astype(jnp.float32)
-    # timestep, x, y, speed, direction
+    # 0 means no collision, 1 means collision
+    collision_branch = jax.lax.select(True, 0., 0.).astype(jnp.float32)
     last_collision_data = jnp.array([0,x,y,speed,direction])
+
+    # check if the ball has hit the boundary
+    hit_boundary = (x <= 0) | (x >= mi.scene_dim[0] - diameter)
 
     # check visibility condition
     is_target_hidden = is_ball_fully_hidden(x, y, diameter, masked_occluders)
@@ -36,17 +39,12 @@ def init_model(mi):
         y=y, 
         speed=speed, 
         direction=direction, 
+        hit_boundary=hit_boundary,
         masked_occluders=masked_occluders, 
-        masked_barriers=masked_barriers, 
         is_target_hidden=is_target_hidden,
         is_target_partially_hidden=is_target_partially_hidden, 
-        is_target_visible=is_target_visible, 
-        red_sensor=mi.red_sensor, 
-        green_sensor=mi.green_sensor, 
+        is_target_visible=is_target_visible,
         collision_branch=collision_branch, 
         last_collision_data=last_collision_data,
-        edgemap=edgemap, 
-        cornermap=cornermap, 
         T=jax.lax.select(True, 0, 0).astype(jnp.int32),
-        stopped_early=jax.lax.select(False, False, False).astype(jnp.bool)
     )
